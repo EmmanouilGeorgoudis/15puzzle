b2e713882f821b58eda7f3c03edf291b4b49a47c
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2025-11-05 19:59:10 +0100

    First commit. Fully functional program

---
+++ src/ButtonList.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ButtonList {
+
+
+    private final List<JButton> buttons = new ArrayList<>();
+    private final List<String> displayNumbers = new ArrayList<>(Arrays.asList("1", "2", "3", "4",
+            "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", ""));
+
+    public ButtonList() {
+        for (int i = 0; i < 16; i++) {
+            JButton b = new JButton(displayNumbers.get(i));
+            b.putClientProperty("name", "b" + (i + 1));
+            buttons.add(b);
+        }
+    }
+
+    public List<JButton> getButtons() {
+        return buttons;
+    }
+
+    public void randomizer() {
+        Collections.shuffle(displayNumbers);
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(displayNumbers.get(i));
+        }
+    }
+
+    public boolean hasEmptyNearby(JButton pressedButton, List<JButton> buttons) {
+
+        int indexOfPressedButton = buttons.indexOf(pressedButton);
+
+        if (buttons.get(indexOfPressedButton).getText().isBlank()) {
+            return false;
+        }
+
+        int indexOfEmpty = -1;
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).getText().isBlank()) {
+                indexOfEmpty = i;
+            }
+        }
+
+        return switch (indexOfPressedButton) {
+            case 0 -> (indexOfEmpty == 1 || indexOfEmpty == 4);
+            case 1 -> (indexOfEmpty == 0 || indexOfEmpty == 2 || indexOfEmpty == 5);
+            case 2 -> (indexOfEmpty == 1 || indexOfEmpty == 3 || indexOfEmpty == 6);
+            case 3 -> (indexOfEmpty == 2 || indexOfEmpty == 7);
+            case 4 -> (indexOfEmpty == 0 || indexOfEmpty == 5 || indexOfEmpty == 8);
+            case 5 -> (indexOfEmpty == 1 || indexOfEmpty == 4 || indexOfEmpty == 6 || indexOfEmpty == 9);
+            case 6 -> (indexOfEmpty == 2 || indexOfEmpty == 5 || indexOfEmpty == 7 || indexOfEmpty == 10);
+            case 7 -> (indexOfEmpty == 3 || indexOfEmpty == 6 || indexOfEmpty == 11);
+            case 8 -> (indexOfEmpty == 4 || indexOfEmpty == 9 || indexOfEmpty == 12);
+            case 9 -> (indexOfEmpty == 5 || indexOfEmpty == 8 || indexOfEmpty == 10 || indexOfEmpty == 13);
+            case 10 -> (indexOfEmpty == 6 || indexOfEmpty == 9 || indexOfEmpty == 11 || indexOfEmpty == 14);
+            case 11 -> (indexOfEmpty == 7 || indexOfEmpty == 10 || indexOfEmpty == 15);
+            case 12 -> (indexOfEmpty == 8 || indexOfEmpty == 13);
+            case 13 -> (indexOfEmpty == 9 || indexOfEmpty == 12 || indexOfEmpty == 14);
+            case 14 -> (indexOfEmpty == 13 || indexOfEmpty == 10 || indexOfEmpty == 15);
+            case 15 -> (indexOfEmpty == 11 || indexOfEmpty == 14);
+            default -> false;
+        };
+    }
+
+    public void numberExchanger(JButton pressedButton, List<JButton> buttons) {
+
+        while (hasEmptyNearby(pressedButton, buttons)) {
+
+            int indexOfPressedButton = buttons.indexOf(pressedButton);
+            int indexOfEmpty = -1;
+
+            for (int i = 0; i < buttons.size(); i++) {
+                if (buttons.get(i).getText().isBlank()) {
+                    indexOfEmpty = i;
+                }
+            }
+
+            String pressedButtonNumber = buttons.get(indexOfPressedButton).getText();
+            String emptyButton = buttons.get(indexOfEmpty).getText();
+
+            buttons.get(indexOfPressedButton).setText(emptyButton);
+            buttons.get(indexOfEmpty).setText(pressedButtonNumber);
+        }
+    }
+
+    public boolean isComplete() {
+
+        for (int i = 0; i < buttons.size(); i++) {
+            if (!buttons.get(i).getText().equals(displayNumbers.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
---
+++ src/GameWindow.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class GameWindow extends JFrame implements ActionListener {
+
+    ButtonList buttonList = new ButtonList();
+    List<JButton> buttons = buttonList.getButtons();
+    JButton resetButton;
+    boolean justReset = false;
+
+    public GameWindow() {
+        this.setTitle("Namn?");
+
+        JPanel gamePanel = new JPanel(new GridLayout(4, 4));
+        JPanel controlPanel = new JPanel();
+
+        for (JButton b : buttons) {
+            b.addActionListener(this);
+            gamePanel.add(b);
+        }
+
+        JLabel timer = new JLabel("");
+        resetButton = new JButton("Nytt spel");
+        resetButton.addActionListener(this);
+
+        controlPanel.add(timer);
+        controlPanel.add(resetButton);
+
+        setLayout(new BorderLayout());
+        add(gamePanel, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+
+
+        this.setSize(400, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        Object source = ae.getSource();
+
+        if (source == resetButton) {
+            buttonList.randomizer();
+            justReset = true;
+        }
+
+        for (int i = 0; i < buttons.size(); i++) {
+            if (source == buttons.get(i)) {
+                buttonList.numberExchanger(buttons.get(i), buttons);
+            }
+        }
+
+        if (!justReset && buttonList.isComplete()) {
+            JOptionPane.showMessageDialog(this, "Perkele Satana!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
